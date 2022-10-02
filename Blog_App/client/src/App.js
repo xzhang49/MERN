311356f8455b747ab56a8745a557825a57babe7c
xzhang49 <xzhanguh@gmail.com>
2022-10-01 21:06:21 -0500

    2nd Edition

--- Blog_App/client/src/App.js
+++ Blog_App/client/src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Single from './pages/Single';
-import Write from './pages/Write';
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import './style.scss';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div><Home /></div>
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
   {
     path: "/register",
@@ -24,20 +49,14 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
   },
-  {
-    path: "/single",
-    element: <Single />,
-  },
-  {
-    path: "/write",
-    element: <Write />,
-  },
-])
+]);
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
--- Blog_App/client/src/pages/Login.jsx
+++ Blog_App/client/src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='auth'>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder='username' />
+        <input type="password" placeholder='passoword' />
+        <button>Login</button>
+        <p>This is an error!</p>
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
   )
 }
 
