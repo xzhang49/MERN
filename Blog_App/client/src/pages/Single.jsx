cc480072e6cfa4180db50d7636c4b6a7538d2f1a
xzhang49 <xzhanguh@gmail.com>
2022-10-02 18:47:12 -0500

    3rd Edition

--- Blog_App/client/src/pages/Single.jsx
+++ Blog_App/client/src/pages/Single.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Edit from '../img/edit.png';
+import Delete from '../img/delete.png';
+
+import { Link } from 'react-router-dom';
+import Menu from '../components/Menu';
 
 function Single() {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className='content'>
+        <img src='' alt=''/>
+        <div className='user'>
+          <img src='' alt=''/>
+          <div className='info'>
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className='edit'>
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <Link>
+              <img src={Delete} alt="" />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className='menu'>
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste dicta minus sapiente omnis laboriosam quasi, suscipit pariatur, voluptas et est aliquid maxime, consectetur odit. Ut doloribus necessitatibus accusamus corporis assumenda.</p>
+      </div>
+      <Menu />
+    </div>
   )
 }
 
